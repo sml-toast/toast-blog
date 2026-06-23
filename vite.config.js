f168ba700dfabbcc6f706fc451bc3d78f8500713
lzw <326011909@qq.com>
2026-06-23 22:02:55 +0800

    Extract environment config tab and use relative paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 export default defineConfig({
   // Use root base for development so that localhost:5174 serves correctly.
-  base: '/',
+  base: './',
   server: { 
     port: 5174,
     host: '0.0.0.0',
